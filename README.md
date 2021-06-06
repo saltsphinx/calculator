a49192ccb1499870ea0bd3752a11149b11489dfd
thomas <tremar.thomas19@gmail.com>
2021-06-06 10:24:48 -0500

    Finished calculator functions, and created basic html and css.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # calculator
+
+How does a calculator work at first glance?
+It requires at the very least two numbers and one operator,
+In cases where there will be a sum, there must be one less
+operator than the numbers, always, for example:
+[9, 45, 12], [+, *] or [6, 18, 32, 86, 94], [+, +, /, +]
+
+Add the numbers and operators to already declared array variables,
+On clicking the equal sign, check to see if theres a number in the input AND
+that theres at least one number and one operator. If fail, return,
+if success, push the input number into the number array and call
+calculation function.
+
+There may be cases where the user has entered an operator before clicking
+the equal sign, in an else statement, check if there are at least two numbers, and
+at least one operator MINUS ONE the length of the operator array. If false,
+return. If true, pop the last added operator and call calculation function.
--- script.js
+++ script.js
@@ -15,10 +15,144 @@ function divide(num, num1) {
 }
 
 function operate(num, num1, op) {
-    const returnResult;
+    let returnResult;
     op === '+' ? returnResult = add(num, num1) :
     op === '-' ? returnResult = subtract(num, num1) :
     op === '*' ? returnResult = multiply(num, num1) :
     op === '/' ? returnResult = divide(num, num1) : false;
-    console.log(returnResult)
+    return returnResult;
+}
+
+const numberButtons = document.querySelectorAll(`.number-button`)
+const operationButtons = document.querySelectorAll(`.operation-button`)
+const clearButton = document.querySelector(`.clear-button`)
+const declareButton = document.querySelector(`.declare-button`)
+const calculatorScreen = document.querySelector(`.calculator-screen`)
+
+let currentInput = ''
+let numberArray = [];
+let operationArray = [];
+
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', e => {
+        const inputStr = e.target.textContent
+        currentInput += inputStr
+        calculatorScreen.textContent = currentInput;
+    })
+}
+
+for (let i = 0; i < operationButtons.length; i++) {
+    operationButtons[i].addEventListener('click', e => {
+        const inputStr = e.target.textContent
+        if (inputStr === '.') checkPeriod();
+        operationArrayHandler(inputStr)
+    })
+}
+
+function currentInputHandler() {
+    numberArray.push(currentInput)
+    currentInput = ''
+}
+
+function checkPeriod() {
+    if (currentInput.indexOf('.')){
+        return
+    }
+    currentInput += '.'
+}
+
+function clearInput() {
+    currentInput = ''
+    console.log('pushed')
+}
+
+function operationArrayHandler(str) {
+    if (currentInput) {
+        currentInputHandler()
+        if (numberArray.length > 0) {
+            console.log('here')
+            if (operationArray.length + 1 <= numberArray.length) {
+                operationArray.push(str)
+            }
+        }
+    } else console.log('no');
+}
+
+// function calculate() {
+//     const operationAmount = operationArray.length
+//     let i = 0;
+//     const calculation = operationArray.reduce((total, operationArray) => {
+//         const product = operate(+numberArray[0], +numberArray[1], operationArray[0])
+//         console.log(`${numberArray[0]} ${operationArray[i]} ${numberArray[1]} = ${product}`)
+//         numberArray.shift()
+//         numberArray.shift()
+//         numberArray.unshift(product)
+//         return product
+//     }, 0)
+//     clearInputs()
+//     calculatorScreen.textContent = calculation
+// }
+
+function calculate() {
+    const operationAmount = operationArray.length
+    let i = 0;
+    const calculation = operationArray.reduce((total, operationArray) => {
+        let firstNum;
+        if (total) {
+            firstNum = total
+        } else {
+            firstNum = +numberArray[0]
+            numberArray.shift()
+        }
+        const product = operate(firstNum, +numberArray[0], operationArray[0])
+        console.log(`${firstNum} ${operationArray[i]} ${numberArray[0]} = ${product}`)
+        numberArray.shift()
+        return product
+    }, 0)
+    clearInputs()
+    calculatorScreen.textContent = calculation
+}
+
+clearButton.addEventListener('click', clearInputs)
+
+function clearInputs() {
+    currentInput = ''
+    numberArray = []
+    operationArray = []
+    calculatorScreen.textContent = '|';
+}
+
+declareButton.addEventListener('click', () => {
+    if (numberArray.length >=1 && operationArray.length >=1) {
+        if (currentInput) {
+            if (currentInput) numberArray.push(currentInput);
+            clearInput()
+            console.log('current input')
+            if (operationArray.length === numberArray.length - 1) {
+                console.log('first')
+                calculate()
+            } else {
+                if (operationArray.length - 1 === numberArray.length - 1) operationArray.pop();
+                if (operationArray.length === numberArray.length - 1) {
+                    console.log('op pop?')
+                    calculate()
+                } else {
+                    console.log(`First number array: ${numberArray}. operation array" ${operationArray}`)
+                }
+            }
+        }
+        else if (operationArray.length === numberArray.length - 1) {
+            console.log('second')
+            calculate()
+        } 
+        else {
+            console.log(`Second number array: ${numberArray}. operation array" ${operationArray}`)
+        }
+    }
+})
+
+function checkit() {
+    console.log(currentInput)
+    console.log(numberArray)
+    console.log(operationArray)
 }
